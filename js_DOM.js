7037d9b65e5914050cc833ddf59cd968ef3ef601
OscarFabiani <spartan539@yahoo.com>
2019-02-06 14:32:49 -0800

    first commit

---
+++ js_DOM.js
@@ -0,0 +1,36 @@
+/*
+This file explores the Document Object Model
+
+DOM: The W3C Document Object Model is a platform and language-neutral interface that allows programs and scripts to dynamically access and
+update the content, structure, and style of a document. The DOM interface defines HTML elements as objects, properties of all HTML elements,
+methods to access all HTML elements, and events for all HTML elements.
+
+DOM navigation: An HTML page is represented as a node tree with the document itself as the root of the tree.
+
+Nodes: There are 6 different types of nodes in the DOM. These are the document node, root node, element node, text node, attribute node, and
+comment node.
+
+Document node: This is the entire document.
+
+Root node: This is the <html> tag.
+
+Element node: Every HTML element.
+
+Text node: text inside HTML.
+
+Attribute node: Every HTML attribute.
+
+Comment node: All comments in the document.
+
+
+DOM and JavaScript: JavaScript uses the DOM as a model or notion of web pages, HTML documents, XML documents, and there component
+parts(elements). Every element in a document is part of the Document Object Model for that document and can be accessed and manipulated
+using the DOM and a scripting language like JavaScript.
+
+
+
+
+
+
+
+*/
